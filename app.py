414f1c1588e0ed9c685cd3a78d32b66e901fce28
Eugene <31552534+ejiang3308@users.noreply.github.com>
2022-06-05 14:09:50 -0700

    Update app.py
    commit the changes to fix the coding issues from pylint

--- app.py
+++ app.py
@@ -1,37 +1,55 @@
 #!/usr/bin/python3
+"""
+This code is used as an example for the Chapter10 of the bookDevOps With Linux
+"""
 
-from flask import Flask, request, jasonify
 from functools import wraps
+from flask import Flask, request, jsonify
 
-app = Flask(__name__)
+APP = Flask(__name__)
 
-def check_card(f):
-    data = request.get_json()
-    if not data.get("status"):
-        response = {"approved":False,
-        "newLimit":data.get("limit"),
-        "reason":"Blocked Card"}
-        return jasonify(response)
 
-    if data.get("limit") < data.get("transaction").get("amount"):
-        response = {"approved":False,
-        "newLimit":data.get("limit"),
-        "reason":"Transaction avove the limit"}
-        return jasonify(*args, **kwargs)
+def check_card(func):
+    """
+    This function validates the credit card transactions
+    """
+    wraps(func)
 
-    return(validation)
+    def validation(*args, **kwargs):
+        """
+        This function is a decorator, which will
+        return the function corresponding to the respective action
+        """
+        data = request.get_json()
+        if not data.get("status"):
+            response = {"approved": False,
+                        "newLimit": data.get("limit"),
+                        "reason": "Blocked Card"}
+            return jsonify(response)
 
-@app.route("/api/transactioni", methods=["POST"])
+        if data.get("limit") < data.get("transaction").get("amount"):
+            response = {"approved": False,
+                        "newLimit": data.get("limit"),
+                        "reason": "Transaction avove the limit"}
+            return jsonify(response)
+        return jsonify(*args, **kwargs)
+    return validation
+
+
+@APP.route("/api/transactioni", methods=["POST"])
 @check_card
-
 def transaction():
+    """
+    This function is resposible to expose the endpoint
+    for receivingthe incoming transactions
+    """
     card = request.get_json()
     new_limit = card.get("limit")
     card.get("transaction").get("amount")
-    response = {"approved":True,
-    "newLimit":new_limit}
-    return jasonify(response)
+    response = {"approved": True,
+                "newLimit": new_limit}
+    return jsonify(response)
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
+    APP.run(debug=True)
